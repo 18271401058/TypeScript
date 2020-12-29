78792af74f1bc2b7f52644f479c69aebb2c4f39b
changpu <943421434@qq.com>
2020-12-29 22:45:14 +0800

    fix: 观察者模式

--- src/2020-12-21.ts
+++ src/2020-12-21.ts
@@ -16,7 +16,7 @@ class Product {
     }
 }
 
-// 这是一个工厂 替代new 操作    // 项目中生成字段
+// 这是一个工厂 替代new 操作    // 根据Type 生成不同类型的字段
 class Creator {
     constructor (name) {
         return new Product(name);
